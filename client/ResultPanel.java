5d3e81f6635da15d2fe446b4753137a32a3272a0
seya-g <affe.on6@gmail.com>
2017-05-20 00:31:00 +0900

    勝敗理由による分岐処理の追加
    ラベルの設定とパンダメソッドの呼び出しを追加

--- client/ResultPanel.java
+++ client/ResultPanel.java
@@ -17,25 +17,60 @@ public class ResultPanel extends JPanel {
 	public static final int FINISH_BY_OPPONENTS_COM_FAILURE =16;
 	
 	private JLabel resultLabel;
+	private JLabel resultImage;
 	
 	public ResultPanel(GameView gameview, GameController gameController) {
 		this.gameView = gameview;
 		this.gameController = gameController;
 		
 		resultLabel = new JLabel();
+		resultImage = new JLabel();
+
+		// 座標で位置を指定するためにレイアウトマネージャを無効にする。
+		setLayout(null);
 	}
 	
 	public void setReason(int reason) {
 		// ゲームが終了した理由をセットする．
 		switch (reason) {
 		// 終了理由別に処理分岐
-		case FINISH_BY_PASS_WIN: break;
-		case FINISH_BY_PASS_LOSE: break;
-		case FINISH_BY_MY_TIMEOUT: break;
-		case FINISH_BY_OPPONENTS_TIMEOUT: break;
-		case FINISH_BY_MY_COM_FAILURE: break;
-		case FINISH_BY_OPPONENTS_COM_FAILURE: break;
+		case FINISH_BY_PASS_WIN:
+			resultLabel.setText("配置できるマスがないため、あなたの勝ちです");
+			//resultImage.setIcon(対応するパンダメソッド);メソッドの戻り値はImageIconクラスの変数
+			break;
+		case FINISH_BY_PASS_LOSE:
+			resultLabel.setText("配置できるマスがないため、あなたの負けです");
+            //resultImage.setIcon(対応するパンダメソッド);メソッドの戻り値はImageIconクラスの変数
+
+
+            break;
+		case FINISH_BY_MY_TIMEOUT:
+			resultLabel.setText("制限時間を超えたため、あなたの負けです");
+            //resultImage.setIcon(対応するパンダメソッド);メソッドの戻り値はImageIconクラスの変数
+
+            break;
+		case FINISH_BY_OPPONENTS_TIMEOUT:
+			resultLabel.setText("対戦相手が制限時間を超えたため、あなたの勝ちです");
+            //resultImage.setIcon(対応するパンダメソッド);メソッドの戻り値はImageIconクラスの変数
+
+            break;
+		case FINISH_BY_MY_COM_FAILURE:
+			resultLabel.setText("接続が切れたため、あなたの負けです");
+            //resultImage.setIcon(対応するパンダメソッド);メソッドの戻り値はImageIconクラスの変数
+
+            break;
+		case FINISH_BY_OPPONENTS_COM_FAILURE:
+			resultLabel.setText("対戦相手の接続が切れたため、あなたの勝ちです");
+            //resultImage.setIcon(対応するパンダメソッド);メソッドの戻り値はImageIconクラスの変数
+
+            break;
 		
 		}
+        //resultLabel.setBounds();  ラベルの位置指定
+        //resultImage.setBounds();
+        add(resultLabel);
+		add(resultImage);
+
+
 	}
 }
